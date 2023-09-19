9fe7761f5e2134be15c13b92a4bac3d30eb76439
Liam-Coulson <coulsonl2209@gmail.com>
2023-09-19 12:10:38 +0100

    Display book info in basic grid within flexbox

--- script.js
+++ script.js
@@ -24,27 +24,33 @@ function addBookToLibrary(bookObj, library=myLibrary) {
 function displayLibrary(library) {
   // Gets the list of current displayed books so we don't double display
   let currentDisplayedLibrary = document.querySelectorAll(".book");
-  let libraryIDList = [];
+  let displayLibraryIDList = [];
   currentDisplayedLibrary.forEach(book => {
-    libraryIDList.push(book.id);
+    displayLibraryIDList.push(book.id);
   })
 
-  console.log(currentDisplayedLibrary);
-
+  // Updates the displayed library in the website
   myLibrary.forEach(bookID => {
-    if (libraryIDList.includes("b"+bookID)) {}
+    if (displayLibraryIDList.includes("b"+bookID)) {}
     else {
-      console.log(`the ID should be valid now, id is ${bookID}`);
       let newBookNode = document.createElement("div");
       newBookNode.id = "b"+bookID; // CSS IDs cannot start with a num
       newBookNode.classList.add("book");
-      newBookNode.textContent = `Title: ${BookList[bookID].title}\n
-      Author: ${BookList[bookID].author}\n
-      Year published: ${BookList[bookID].year}\n
-      Pages: ${BookList[bookID].pages}\n
-      Genre: ${BookList[bookID].genre}\n
-      Have I read it: ${BookList[bookID].read}
-      `;
+      Object.keys(BookList[bookID]).forEach(field => {
+        const fieldNode = document.createElement("div");
+        fieldNode.textContent = field;
+        const valueNode = document.createElement("div");
+        valueNode.textContent = BookList[bookID][field];
+        newBookNode.appendChild(fieldNode);
+        newBookNode.appendChild(valueNode);
+      })
+      // newBookNode.textContent = `Title: ${BookList[bookID].title}\n
+      // Author: ${BookList[bookID].author}\n
+      // Year published: ${BookList[bookID].year}\n
+      // Pages: ${BookList[bookID].pages}\n
+      // Genre: ${BookList[bookID].genre}\n
+      // Have I read it: ${BookList[bookID].read}
+      // `;
       libraryWrapper.appendChild(newBookNode);
     }
   })
@@ -55,7 +61,5 @@ addBookToLibrary(new Book("The Hunger Games", "Suzanne Collins", "2008", "374", 
 addBookToLibrary(new Book("Dune", "Frank Herbert", "1965", "755", "Science Fiction", "yes"));
 addBookToLibrary(new Book("A Christmas Carol", "Charles Dickens", "1843", "65", "Victorian Literature", "no"));
 
-console.log(BookList);
 
-displayLibrary(myLibrary);
 displayLibrary(myLibrary);
