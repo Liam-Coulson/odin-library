b9bf58c7fa0ca8def3a13a23ee1218d5301e5f50
Liam-Coulson <coulsonl2209@gmail.com>
2023-09-20 18:55:31 +0100

    Allow user to edit book information with edit btn

--- script.js
+++ script.js
@@ -3,16 +3,28 @@ const headerRow = document.getElementById("header-row");
 const addButton = document.getElementById("add");
 const editButton = document.getElementById("edit");
 const deleteButton = document.getElementById("delete");
+const buttonsContainer = addButton.parentNode;
 
 const myLibrary = [];
 const BookList = {};
 
-// EVENTS
+// ==========================================
+// ================= EVENTS =================
+// ==========================================
+
 libraryWrapper.addEventListener("click", clickHandler);
 addButton.addEventListener("click", clickHandler);
 editButton.addEventListener("click", clickHandler);
 deleteButton.addEventListener("click", clickHandler);
 
+document.addEventListener("keydown", key => {
+  if (key.code == "Enter") {
+    let editingBook = libraryWrapper.querySelector(".editing");
+    if (editingBook != null) {
+      finishEditing(editingBook);
+    } 
+  }
+})
 
 function clickHandler(e) {
   if (e.target.parentNode.matches(".book")) {
@@ -24,31 +36,64 @@ function clickHandler(e) {
       })
       e.target.parentNode.classList.add("selected");
     }
-  } else if (e.target.matches("#add")) {
+  }
+  else if (e.target.matches("#add")) {
     console.log("Adding new book...");
-  } else if (e.target.matches("#edit")) {
-    console.log("Editing this book...");
-  } else if (e.target.matches("#delete")) {
+  }
+  else if (e.target.matches("#edit")) {
+    let selectedBook = libraryWrapper.querySelector(".selected");
+    selectedBook.classList.add("editing");
+    let bookID = selectedBook.id.slice(1);
+    console.log(`Editing this book: ${BookList[bookID].title}`);
+    // Remove click events from all books while editing is in progress
+    libraryWrapper.removeEventListener("click", clickHandler);
+    selectedBook.childNodes.forEach(gridElm => {
+      if (gridElm.classList.contains("value")) {
+        gridElm.contentEditable = true;
+
+        // Credit for this function from stackoverflow https://stackoverflow.com/a/3806004
+        // This allows the user to tab between fields and auto-select all text in the fields
+        gridElm.onfocus = function() {
+          window.setTimeout(function() {
+              var sel, range;
+              if (window.getSelection && document.createRange) {
+                  range = document.createRange();
+                  range.selectNodeContents(gridElm);
+                  sel = window.getSelection();
+                  sel.removeAllRanges();
+                  sel.addRange(range);
+              } else if (document.body.createTextRange) {
+                  range = document.body.createTextRange();
+                  range.moveToElementText(gridElm);
+                  range.select();
+              }
+          }, 1);
+        };
+
+        gridElm.classList.add("editable");
+      }
+    })
+    buttonsContainer.childNodes.forEach(button => {
+      button.disabled = true;
+    })
+    selectedBook.querySelector(".editable").focus();
+
+  }
+  else if (e.target.matches("#delete")) {
     if (libraryWrapper.querySelector(".selected") == null) {
       console.log("You haven't selected a book yet.")
     } else {
       let selectedBook = libraryWrapper.querySelector(".selected");
-      let bookID = selectedBook.id.slice(1); // Remove the leading "b"
-      console.log(`Deleting this book: ${BookList[bookID].title}`);
-      console.log(`My library looks like this: ${myLibrary}`);
-      console.log(`My library index 1 has type ${typeof myLibrary[1]}`);
-      console.log(`BookID from javascript has type ${typeof bookID}`);
-      console.log(`Index of ${bookID} is ${myLibrary.indexOf(bookID)}`);
+      let bookID = selectedBook.id.slice(1); // Remove the leading "b" from ID
       myLibrary.splice(myLibrary.indexOf(bookID),1); // Delete from JS array
-      console.log("My JS library after deleting from JS: ",myLibrary)
       selectedBook.remove(); // Delete from DOM
-      console.log("The book has been deleted!");
-      myLibrary.forEach(bookID => {
-        console.log(BookList[bookID]);
-      })
     }
   }
 }
+
+// =====================================
+// ============== CLASSES ==============
+// =====================================
 
 class Book {
   constructor(title, author, year, pages, genre, read) {
@@ -89,9 +134,11 @@ function displayLibrary(library) {
 
       Object.keys(BookList[bookID]).forEach(field => {
         const fieldNode = document.createElement("div");
+        fieldNode.classList.add(field);
         // Capitalise the first letter
         fieldNode.textContent = field.charAt(0).toUpperCase()+field.slice(1);
         const valueNode = document.createElement("div");
+        valueNode.classList.add("value");
         valueNode.textContent = BookList[bookID][field];
         newBookNode.appendChild(fieldNode);
         newBookNode.appendChild(valueNode);
@@ -99,6 +146,54 @@ function displayLibrary(library) {
       libraryWrapper.appendChild(newBookNode);
     }
   })
+}
+
+// Reverses all of the changes we made when making the book
+function finishEditing(editingBook) {
+  libraryWrapper.addEventListener("click", clickHandler);
+  editingBook.classList.remove("editing");
+  buttonsContainer.childNodes.forEach(button => {
+    button.disabled = false;
+  })
+  editingBook.childNodes.forEach(gridElm => {
+    if (gridElm.classList.contains("value")) {
+      gridElm.contentEditable = false;
+
+      // Credit for this function from stackoverflow https://stackoverflow.com/a/3806004
+      // This allows the user to tab between fields and auto-select all text in the fields
+      gridElm.onfocus = function() {
+        window.setTimeout(function() {
+            var sel, range;
+            if (window.getSelection && document.createRange) {
+                range = document.createRange();
+                range.selectNodeContents(gridElm);
+                sel = window.getSelection();
+                sel.removeAllRanges();
+                sel.addRange(range);
+            } else if (document.body.createTextRange) {
+                range = document.body.createTextRange();
+                range.moveToElementText(gridElm);
+                range.select();
+            }
+        }, 1);
+      };
+      gridElm.classList.remove("editable");
+    }
+  })
+  changeBookDetails(editingBook);
+}
+
+// Takes the changes in HTML and transfers these changes to JS
+// Without doing this, if we added an "update" button to update the
+// library on screen, our changes would not have been saved.
+function changeBookDetails(selectedBook) {
+  let bookID = selectedBook.id.slice(1);
+  Object.keys(BookList[bookID]).forEach(key => {
+    BookList[bookID][key] = selectedBook.querySelector(`.${key}`).nextElementSibling.textContent;
+  })
+
+  console.log("The new book information is now: ", BookList[bookID]);
+  displayLibrary(myLibrary);
 }
 
 addBookToLibrary(new Book("The Hobbit", "JRR Tolkein", "1937", "310", "Fantasy", "no"));
