3a5250ff83a8f139b2156ea2a0053b449df16311
Liam-Coulson <coulsonl2209@gmail.com>
2023-09-25 11:19:15 +0100

    Refactor edit book code, allow adding new books

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ document.addEventListener("keydown", key => {
     if (editingBook != null) {
       finishEditing(editingBook);
     } 
+  } else if (key.code == "KeyK") {
+    libraryWrapper.querySelector(".book").click();
   }
 })
 
@@ -38,46 +40,17 @@ function clickHandler(e) {
     }
   }
   else if (e.target.matches("#add")) {
-    console.log("Adding new book...");
+    let blankBook = new Book();
+    addBookToLibrary(blankBook);
+    const newBook = createNewBookElm(myLibrary[myLibrary.length-1]);
+    libraryWrapper.appendChild(newBook);
+    toggleButtons();
+    editHTMLBookFields(newBook);
   }
   else if (e.target.matches("#edit")) {
     let selectedBook = libraryWrapper.querySelector(".selected");
-    selectedBook.classList.add("editing");
-    let bookID = selectedBook.id.slice(1);
-    console.log(`Editing this book: ${BookList[bookID].title}`);
-    // Remove click events from all books while editing is in progress
-    libraryWrapper.removeEventListener("click", clickHandler);
-    selectedBook.childNodes.forEach(gridElm => {
-      if (gridElm.classList.contains("value")) {
-        gridElm.contentEditable = true;
-
-        // Credit for this function from stackoverflow https://stackoverflow.com/a/3806004
-        // This allows the user to tab between fields and auto-select all text in the fields
-        gridElm.onfocus = function() {
-          window.setTimeout(function() {
-              var sel, range;
-              if (window.getSelection && document.createRange) {
-                  range = document.createRange();
-                  range.selectNodeContents(gridElm);
-                  sel = window.getSelection();
-                  sel.removeAllRanges();
-                  sel.addRange(range);
-              } else if (document.body.createTextRange) {
-                  range = document.body.createTextRange();
-                  range.moveToElementText(gridElm);
-                  range.select();
-              }
-          }, 1);
-        };
-
-        gridElm.classList.add("editable");
-      }
-    })
-    buttonsContainer.childNodes.forEach(button => {
-      button.disabled = true;
-    })
-    selectedBook.querySelector(".editable").focus();
-
+    toggleButtons();
+    editHTMLBookFields(selectedBook);
   }
   else if (e.target.matches("#delete")) {
     if (libraryWrapper.querySelector(".selected") == null) {
@@ -130,6 +103,50 @@ function displayLibrary(library) {
   })
 }
 
+function toggleButtons() {
+  buttonsContainer.childNodes.forEach(button => {
+    button.disabled = !button.disabled;
+  })
+}
+
+function editHTMLBookFields(bookElm) {
+  bookElm.classList.add("editing");
+  let bookID = bookElm.id.slice(1);
+  // Remove click events from all books while editing is in progress
+  libraryWrapper.removeEventListener("click", clickHandler);
+  bookElm.childNodes.forEach(gridElm => {
+    if (gridElm.classList.contains("value")) {
+      gridElm.contentEditable = true;
+
+      // Credit for this function from stackoverflow https://stackoverflow.com/a/3806004
+      // This allows the user to tab between fields and auto-select all text in the fields
+      gridElm.onfocus = function() {
+        window.setTimeout(function() {
+          var sel, range;
+          if (window.getSelection && document.createRange) {
+            range = document.createRange();
+            range.selectNodeContents(gridElm);
+            sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+          } else if (document.body.createTextRange) {
+            range = document.body.createTextRange();
+            range.moveToElementText(gridElm);
+            range.select();
+          }
+        }, 1);
+      };
+
+      gridElm.classList.add("editable");
+    }
+  })
+  buttonsContainer.childNodes.forEach(button => {
+    button.disabled = true;
+  })
+  bookElm.querySelector(".editable").focus();
+
+}
+
 // Reverses all of the changes we made to the HTML
 // when making the book editable
 function finishEditing(editingBook) {
@@ -156,11 +173,16 @@ function changeBookList(selectedBook) {
     BookList[bookID][key] = selectedBook.querySelector(`.${key}`).nextElementSibling.textContent;
   })
 
-  console.log("The new book information is now: ", BookList[bookID]);
   displayLibrary(myLibrary);
 }
 
-function createNewBookElm(bookID=null) {
+/**
+ * Creates a HTML element for the given book ID. If no ID is given,
+ * then all fields are left empty and available for the user to fill
+ * @param {string} bookID 
+ * @returns HTML element
+ */
+function createNewBookElm(bookID) {
   let newBookNode = document.createElement("div");
   newBookNode.id = "b"+bookID; // CSS IDs cannot start with a num
   newBookNode.classList.add("book");
@@ -168,19 +190,18 @@ function createNewBookElm(bookID=null) {
   const bookSpine = document.createElement("div");
   bookSpine.classList.add("spine");
   newBookNode.appendChild(bookSpine);
-
-  Object.keys(Book).forEach(field => {
+  
+  Object.getOwnPropertyNames(new Book).forEach(field => {
+    
     const fieldNode = document.createElement("div");
     fieldNode.classList.add(field);
     // Capitalise the first letter
     fieldNode.textContent = field.charAt(0).toUpperCase()+field.slice(1);
+    
     const valueNode = document.createElement("div");
     valueNode.classList.add("value");
-    
-    // If book info is present, auto-fill the book element with it
-    if (bookID != null) {
-      valueNode.textContent = BookList[bookID][field];
-    } // else leave it blank
+    valueNode.textContent = BookList[bookID][field];
+
     newBookNode.appendChild(fieldNode);
     newBookNode.appendChild(valueNode);
   })
@@ -194,5 +215,3 @@ addBookToLibrary(new Book("Dune", "Frank Herbert", "1965", "755", "Science Ficti
 addBookToLibrary(new Book("A Christmas Carol", "Charles Dickens", "1843", "65", "Victorian Literature", "no"));
 
 displayLibrary(myLibrary);
-
-console.log(Object.keys(BookList));
